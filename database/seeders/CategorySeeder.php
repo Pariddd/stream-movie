2026a33d7af494297e5e902166b80f4f8a0d1971
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2026-02-03 14:07:37 +0700

    menambahkan seeder untuk table category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Action',
+            'Drama',
+            'Comedy',
+            'Horror',
+            'Thriller',
+            'Romance',
+            'Sci-Fi',
+            'Adventure',
+            'Fantasy',
+            'Documentary',
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+            ]);
+        }
+    }
+}
